e1cae2ed7d2c98618078809088469c275c49d465
bazhukovsi <bazhukovsi@yandex.ru>
2023-01-06 15:51:13 +0300

    4. Схема таблицы Post [#1731]

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -4,13 +4,21 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Post {
-    int id;
-    String title;
-    String link;
-    String description;
-    LocalDateTime created;
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
 
     public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this.id = id;
         this.title = title;
         this.link = link;
         this.description = description;
